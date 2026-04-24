564d68530c7ff72b5905db2f098e426dd884c9c0
beapitt <salvatibeatricebautista@gmail.com>
2026-04-24 11:34:47 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import AdminDashboard from "./pages/AdminDashboard";
 import AdminTemplates from "./pages/AdminTemplates";
 import AdminCategories from "./pages/AdminCategories";
 import Settings from "./pages/Settings";
-import EditTemplate from "./pages/EditTemplate"; // <--- Import nuovo
+import EditTemplate from "./pages/EditTemplate";
+import EditCategory from "./pages/EditCategory";
 import TemplateDetail from "./pages/TemplateDetail";
 
 export default function App() {
@@ -15,14 +16,17 @@ export default function App() {
         <Route path="/template/:slug" element={<TemplateDetail />} />
         <Route path="/admin" element={<AdminDashboard />} />
         
-        {/* Gestione Templates */}
         <Route path="/admin/templates" element={<AdminTemplates />} />
         <Route path="/admin/templates/new" element={<EditTemplate />} />
         <Route path="/admin/templates/edit/:id" element={<EditTemplate />} />
         
         <Route path="/admin/categories" element={<AdminCategories />} />
+        <Route path="/admin/categories/new" element={<EditCategory />} />
+        <Route path="/admin/categories/edit/:id" element={<EditCategory />} />
+        
         <Route path="/admin/settings" element={<Settings />} />
         
+        {/* Prossimamente AdSlots e Legal */}
         <Route path="/admin/ads" element={<div className="p-10 text-left">Ads coming soon...</div>} />
         <Route path="/admin/legal" element={<div className="p-10 text-left">Legal coming soon...</div>} />
       </Routes>
