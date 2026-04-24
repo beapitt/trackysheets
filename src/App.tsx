8effe20a3ec3ff5da0cfc80a6175e49c4c6113ca
beapitt <salvatibeatricebautista@gmail.com>
2026-04-24 12:35:42 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-// Public Pages
+// Public
 import Home from './pages/Home';
 import TemplateDetail from './pages/TemplateDetail';
 
-// Admin Pages
+// Admin - Assicurati che i nomi dei file su GitHub siano ESATTAMENTE questi
 import AdminDashboard from './pages/AdminDashboard';
 import AdminTemplates from './pages/AdminTemplates';
 import EditTemplate from './pages/EditTemplate';
@@ -17,31 +17,25 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Public */}
         <Route path="/" element={<Home />} />
         <Route path="/template/:slug" element={<TemplateDetail />} />
-
-        {/* Admin Dashboard */}
-        <Route path="/admin" element={<AdminDashboard />} />
         
-        {/* Templates */}
+        {/* Admin Section */}
+        <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/admin/templates" element={<AdminTemplates />} />
         <Route path="/admin/templates/new" element={<EditTemplate />} />
         <Route path="/admin/templates/:id/edit" element={<EditTemplate />} />
-
-        {/* Categories */}
+        
         <Route path="/admin/categories" element={<AdminCategories />} />
         <Route path="/admin/categories/new" element={<EditCategory />} />
         <Route path="/admin/categories/:id/edit" element={<EditCategory />} />
-
-        {/* Settings */}
+        
         <Route path="/admin/settings" element={<Settings />} />
 
-        {/* Fallbacks per AdSlots e Legal - Mandano alla dashboard invece che dare errore */}
+        {/* Fallback per AdSlots e Legal che non hai ancora creato */}
         <Route path="/admin/ads" element={<AdminDashboard />} />
         <Route path="/admin/legal" element={<AdminDashboard />} />
 
-        {/* Redirect automatico se la pagina non esiste */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
