39d54bd67de36bc1104126d07a3de3223b98162e
beapitt <salvatibeatricebautista@gmail.com>
2026-04-24 12:23:23 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,49 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import AdminDashboard from "./pages/AdminDashboard";
-import AdminTemplates from "./pages/AdminTemplates";
-import AdminCategories from "./pages/AdminCategories";
-import Settings from "./pages/Settings";
-import EditTemplate from "./pages/EditTemplate";
-import EditCategory from "./pages/EditCategory";
-import TemplateDetail from "./pages/TemplateDetail";
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+// Public Pages
+import Home from './pages/Home';
+import TemplateDetail from './pages/TemplateDetail';
+
+// Admin Pages
+import AdminDashboard from './pages/AdminDashboard';
+import AdminTemplates from './pages/AdminTemplates';
+import EditTemplate from './pages/EditTemplate';
+import AdminCategories from './pages/AdminCategories';
+import EditCategory from './pages/EditCategory';
+import Settings from './pages/Settings';
 
 export default function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
+        {/* Public Routes */}
         <Route path="/" element={<Home />} />
         <Route path="/template/:slug" element={<TemplateDetail />} />
+
+        {/* Admin Dashboard */}
         <Route path="/admin" element={<AdminDashboard />} />
         
-        {/* Templates */}
+        {/* Templates Management */}
         <Route path="/admin/templates" element={<AdminTemplates />} />
         <Route path="/admin/templates/new" element={<EditTemplate />} />
-        <Route path="/admin/templates/edit/:id" element={<EditTemplate />} />
-        
-        {/* Categories */}
+        <Route path="/admin/templates/:id/edit" element={<EditTemplate />} />
+
+        {/* Categories Management */}
         <Route path="/admin/categories" element={<AdminCategories />} />
         <Route path="/admin/categories/new" element={<EditCategory />} />
-        <Route path="/admin/categories/edit/:id" element={<EditCategory />} />
-        
+        <Route path="/admin/categories/:id/edit" element={<EditCategory />} />
+
+        {/* Settings */}
         <Route path="/admin/settings" element={<Settings />} />
-        <Route path="/admin/ads" element={<div className="p-10 text-left">Ads coming soon...</div>} />
-        <Route path="/admin/legal" element={<div className="p-10 text-left">Legal coming soon...</div>} />
+
+        {/* Fallback for development sections */}
+        <Route path="/admin/ads" element={<AdminDashboard />} />
+        <Route path="/admin/legal" element={<AdminDashboard />} />
+
+        {/* Catch-all to Home */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
